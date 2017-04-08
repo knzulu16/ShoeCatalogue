899193ebb30cf292b98a2f054d18d899d20cd10b
Nonzulu <knzulu16@gmail.com>
2017-04-08 14:01:11 +0200

    Added changes

--- shoeCatalogue.js
+++ shoeCatalogue.js
@@ -1,40 +1,192 @@
 
-var shoeData=document.querySelector(".shoeData");
-var unOrdered =document.getElementById('#output');
-var drop=document.querySelector('#drop');
-var myTemplate=document.querySelector('.myTemplate').innerHTML;
-var button = document.querySelector('.search');
 
+var shoes = [
+  {ProductType:'Pump',
+  Brand:'Rubi',
+  Color:'Blue',
+  Size:"5",
+  Price:'R1500.00',
+  in_stock:"5"},
 
-var myTemplateInstance = Handlebars.compile(myTemplate);
+  {
+  ProductType:'Buckles',
+  Brand:'Aldo',
+  Color:'Brown',
+  Size:"6",
+  Price:"R2000",
+  in_stock:"7",
 
-var data = [
-  { brand:'rubi',color:'blue',size:"5",in_stock:"5"},
-  { brand:'aldo',color:'red',size:"6",in_stock:"7"}
+},
+  { ProductType:'Lace-UPS',
+    Brand:'Aldo',
+    Color:'Pink',
+    Size:'7',
+    Price:'R2500.00',
+    in_stock:'8'
+  },
+  { ProductType:'Lace-UPS',
+    Brand:'Lacoste',
+    Color:'Black',
+    Size:'6',
+    Price:'R1500.00',
+    in_stock:'10'
+  },
+  {ProductType:'Sneaker',
+    Brand:'Rubi',
+    Color:'White',
+    Size:'8',
+    Price:'R1200.00',
+    in_stock:'12'
+  }
 ];
 
-var prdctDrpDwn = document.querySelector('#prdctDrpDwn');
-var brandDrpDwn = document.querySelector('#brandDrpDwn');
-var colorDrpDwn = document.querySelector('#colorDrpDwn');
-var sizeDrpDwn = document.querySelector('#sizeDrpDwn');
 
-var brands = document.getElementsByName('brand');
-var colors = document.getElementsByName('color');
-var sizes = document.getElementsByName('size');
+var myTemplate= document.querySelector('#myTemplate').innerHTML;
+var myTemplateInstance = Handlebars.compile(myTemplate);
 
-button.addEventListener('click', function () {
-  if(brandDrpDwn.value === brands[0].textContent && colorDrpDwn.value === colors[0].textContent && sizeDrpDwn.value === sizes[0].textContent)
-  {unOrdered.innerHTML= myTemplateInstance(data[0]);}
-});
-// var value=myTemplateInstance(data);
-// document.getElementById("output").innerHTML=data;
+var results = myTemplateInstance({data:shoes});
+console.log(results);
 
 
+document.querySelector('.shoeDisplay').innerHTML+=results;
 
+
+function dropDown(){
+  var selectedBrandName=document.getElementById('brandDrpDwn');
+    var selectedColor = document.getElementById('colorDrpDwn');
+    var selectedSize= document.getElementById('sizeDrpDwn');
+    console.log(selectedSize);
+    var selectedPrice=document.getElementById('PriceDrDwn');
+
+var filteredList = [];
+for (var i=0;i<shoes.length;i++){
+  var shoes = shoes[i];
+  if(selectedBrandName.value === shoes[i].value &&selectedColor.value === shoes[i].Color &&
+      selectedSize.value === shoes[i].Size && selectedPrice.value===shoes[i].value)
+    {
+
+    filteredList.push(shoes);
+  }
+}
+
+//Now use this with my template:
+var filteredDropDwnHTML = myTemplate({ shoes: filteredList});
+shoeFilter.innerHTML = filteredDropDwnHTML;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This will display the result of your template combined with the data on the screen.
+
+
+
+//
+//
+//
+// //
+// var colorSelect=document.querySelector(".colorTemplate").innerHTML;
+// colorSelect=Handlebars.compile(colorSelect);
+// var sizeSelect=document.querySelector(".sizeTemplate").innerHTML;
+// sizeSelect=Handlebars.compile(sizeSelect);
+// var brandSelect=document.querySelector('.brandTemplate').innerHTML;
+// brandSelect=Handlebars.compile(brandSelect);
+// //
+// function searchButton(){
+// var array=[];
+// for(var i=0;i<shoes.length;i++){
+//   // console.log(data[i]);
+//   if(selectedBrandName.value === shoes[i].Brand &&selectedColor.value === shoes[i].Color &&
+//     selectedSize.value === shoes[i].Size && selectedPrice.value===shoes[i].Price)
+//   {
+//     array.push(shoes[i]);
+//     unOrdered.innerHTML= myTemplateInstance(shoes[i]);
+//
+//   }
+//
+//   // var selectedProduct= document.getElementById('prdctDrpDwn');
+//
+//   var selectedBrandName=document.getElementById('brandDrpDwn');
+//   var selectedColor = document.getElementById('colorDrpDwn');
+//   var selectedSize= document.getElementById('sizeDrpDwn');
+//   var selectedPrice=document.getElementById('PriceDrDwn');
+//   // var brands = document.getElementsByName('brand');
+//   // var colors = document.getElementsByName('color');
+//   // var sizes = document.getElementsByName('size');
+//
+// var list=document.querySelector("data");
+// var unOrdered =document.getElementById('output');
+// // var drop=document.querySelector('#drop');
+// // var myTemplate=document.querySelector('#myTemplate').innerHTML;
+// var templateScript=document.querySelector("#myTemplate").innerHTML;
+// var results=myTemplateInstance({shoeData:data})
+// document.querySelector(".tbody").innerHTML = results;
+// var button = document.querySelector('.search');
+//
+//
+// // button.addEventListener('click', function(e){
+// //   // search.e.target.value;
+// //
+// // });
+//
+//   // console.log(selectedBrandName.value);
+//     // unOrdered[i].style.display='block'
+//             // document.getElementById("output").innerHTML=
+//             // output.innerHTML = myTemplateInstance(data);
+//             // var source = document.getElementById('display').innerHTML;
+//
+//   else{
+//     data[i].style.display = "none";
+//   }
+//
+// }
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // var value=myTemplateInstance(data);
+// // document.getElementById("output").innerHTML=data;
+//
+//
+//
+//
+//
+//
 var myIndex = 0;
-carousel();
 
-function carousel() {
+function carousel(){
     var i;
     var x = document.getElementsByClassName("slideShow");
     for (i = 0; i < x.length; i++) {
@@ -45,35 +197,35 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-
-// function searchShoes(){
-//   const li=document.createElement('li');
-//   li.textContent=text;
-//   ul.appendChild('li');
+carousel();
 //
-// }
-
+// // function searchShoes(){
+// //   const li=document.createElement('li');
+// //   li.textContent=text;
+// //   ul.appendChild('li');
+// //
+// // }
 //
-// drop.addEventListener('change',function(e){
-//   console.log(e.target.value);
-//   var selectedShoe=shoeData.value;
+// //
+// // drop.addEventListener('change',function(e){
+// //   console.log(e.target.value);
+// //   var selectedShoe=shoeData.value;
+// //
+// //   var allShoes=document.querySelectorAll('li');
+// //   var value=myTemplateInstance(data);
+// //     for(var i=0;i<allShoes.length;i++){
+// //       if(allShoes[i].innerHTML.startsWith(e.target.value)){
+// //       }
+// //     }
+// // });
 //
-//   var allShoes=document.querySelectorAll('li');
-//   var value=myTemplateInstance(data);
-//     for(var i=0;i<allShoes.length;i++){
-//       if(allShoes[i].innerHTML.startsWith(e.target.value)){
-//         document.getElementById("output").innerHTML=shoeData.value;
-//       }
-//     }
-// });
-
-
-
-// console.log(allShoes);
-//     document.getElementById('li').innerHTML=li.value;
-// }
-// else{
-//   allShoes[i].style.display='none'
-// }
-// }
-// });
+//
+//
+// // console.log(allShoes);
+// //     document.getElementById('li').innerHTML=li.value;
+// // }
+// // else{
+// //   allShoes[i].style.display='none'
+// // }
+// // }
+// // });
